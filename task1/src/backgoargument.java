185455f16be3d55a7a008c85c47dadee6dd9919d
Pavel Siamionau <pavelms@gmail.com>
2019-07-24 21:35:01 +0300

    forgot to add comment to the code

--- task1/src/backgoargument.java
+++ task1/src/backgoargument.java
@@ -1,9 +1,9 @@
-import java.util.ArrayList;
+import java.util.ArrayList; //import ArrayList
 class backgoargument {
     public static void main(String[] args) {
 
 
-        for (int i=args.length -1; i>=0; i--){
+        for (int i=args.length -1; i>=0; i--){ //Revers Array
 
             System.out.println("Argument : " + args[i]);
         }
