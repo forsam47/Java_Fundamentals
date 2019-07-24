8dd6bd96ed2d46e59541e80d6a10340183bd08d6
Pavel Siamionau <pavelms@gmail.com>
2019-07-24 20:34:48 +0300

    add second task

---
+++ task1/src/backgoargument.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+class backgoargument {
+    public static void main(String[] args) {
+
+
+        for (int i=args.length -1; i>=0; i--){
+
+            System.out.println("Argument : " + args[i]);
+        }
+    }
+}
